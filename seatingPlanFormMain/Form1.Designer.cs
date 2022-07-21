330d2ec47f5abee9719b7908de583f2db209b281
James Caroe <83098125+James46113@users.noreply.github.com>
2022-07-21 14:02:54 +0100

    Add project files.

---
+++ seatingPlanFormMain/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace seatingPlanFormMain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.importStudents = new System.Windows.Forms.Button();
+            this.generatePlan = new System.Windows.Forms.Button();
+            this.addStudent = new System.Windows.Forms.Button();
+            this.clearStudents = new System.Windows.Forms.Button();
+            this.addBox = new System.Windows.Forms.TextBox();
+            this.studentsBox = new System.Windows.Forms.ListBox();
+            this.shuffledBox = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // importStudents
+            // 
+            this.importStudents.Location = new System.Drawing.Point(12, 12);
+            this.importStudents.Name = "importStudents";
+            this.importStudents.Size = new System.Drawing.Size(75, 23);
+            this.importStudents.TabIndex = 0;
+            this.importStudents.Text = "importStudents";
+            this.importStudents.UseVisualStyleBackColor = true;
+            this.importStudents.Click += new System.EventHandler(this.importStudents_Click);
+            // 
+            // generatePlan
+            // 
+            this.generatePlan.Location = new System.Drawing.Point(12, 41);
+            this.generatePlan.Name = "generatePlan";
+            this.generatePlan.Size = new System.Drawing.Size(75, 23);
+            this.generatePlan.TabIndex = 1;
+            this.generatePlan.Text = "generatePlan";
+            this.generatePlan.UseVisualStyleBackColor = true;
+            this.generatePlan.Click += new System.EventHandler(this.generatePlan_Click);
+            // 
+            // addStudent
+            // 
+            this.addStudent.Location = new System.Drawing.Point(12, 70);
+            this.addStudent.Name = "addStudent";
+            this.addStudent.Size = new System.Drawing.Size(75, 23);
+            this.addStudent.TabIndex = 2;
+            this.addStudent.Text = "addStudent";
+            this.addStudent.UseVisualStyleBackColor = true;
+            this.addStudent.Click += new System.EventHandler(this.addStudent_Click);
+            // 
+            // clearStudents
+            // 
+            this.clearStudents.Location = new System.Drawing.Point(12, 99);
+            this.clearStudents.Name = "clearStudents";
+            this.clearStudents.Size = new System.Drawing.Size(75, 23);
+            this.clearStudents.TabIndex = 3;
+            this.clearStudents.Text = "clearStudents";
+            this.clearStudents.UseVisualStyleBackColor = true;
+            this.clearStudents.Click += new System.EventHandler(this.clearStudents_Click);
+            // 
+            // addBox
+            // 
+            this.addBox.Location = new System.Drawing.Point(12, 128);
+            this.addBox.Name = "addBox";
+            this.addBox.Size = new System.Drawing.Size(100, 20);
+            this.addBox.TabIndex = 4;
+            this.addBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.addBox_KeyDown);
+            // 
+            // studentsBox
+            // 
+            this.studentsBox.FormattingEnabled = true;
+            this.studentsBox.Location = new System.Drawing.Point(12, 154);
+            this.studentsBox.Name = "studentsBox";
+            this.studentsBox.Size = new System.Drawing.Size(120, 95);
+            this.studentsBox.TabIndex = 5;
+            this.studentsBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.studentsBox_MouseDoubleClick);
+            // 
+            // shuffledBox
+            // 
+            this.shuffledBox.FormattingEnabled = true;
+            this.shuffledBox.Location = new System.Drawing.Point(12, 255);
+            this.shuffledBox.Name = "shuffledBox";
+            this.shuffledBox.Size = new System.Drawing.Size(120, 95);
+            this.shuffledBox.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.shuffledBox);
+            this.Controls.Add(this.studentsBox);
+            this.Controls.Add(this.addBox);
+            this.Controls.Add(this.clearStudents);
+            this.Controls.Add(this.addStudent);
+            this.Controls.Add(this.generatePlan);
+            this.Controls.Add(this.importStudents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button importStudents;
+        private System.Windows.Forms.Button generatePlan;
+        private System.Windows.Forms.Button addStudent;
+        private System.Windows.Forms.Button clearStudents;
+        private System.Windows.Forms.TextBox addBox;
+        private System.Windows.Forms.ListBox studentsBox;
+        private System.Windows.Forms.ListBox shuffledBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ seatingPlanFormMain/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace seatingPlanFormMain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            studentsBox.Items.Clear();
+            students.Clear();
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            using (StreamReader reader = new StreamReader(files[0]))
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    studentsBox.Items.Add(line);
+                    students.Add(line);
+                }
+            }
+        }
+
+
+
+        List<string> students = new List<string>();
+        Random rnd = new Random();
+
+        void getStudents()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                getStudentsFile();
+            }
+        }
+        void getStudentsFile()
+        {
+            try
+            {
+                using (StreamReader sReader = new StreamReader(openFileDialog1.FileName))
+                {
+                    string line;
+                    while (true)
+                    {
+                        line = sReader.ReadLine();
+                        if (line != null)
+                        {
+                            studentsBox.Items.Add(line);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void shuffleStudents()
+        {
+            students.Clear();
+            foreach (string item in studentsBox.Items)
+            {
+                students.Add(item);
+            }
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                int rnum = rnd.Next(0, students.Count);
+                string temp = students[i];
+                students[i] = students[rnum];
+                students[rnum] = temp;
+            }
+            shuffledBox.Items.Clear();
+            foreach (string item in students)
+            {
+                shuffledBox.Items.Add((students.IndexOf(item) + 1) + " - " + item);
+                //Console.WriteLine(item);
+            }
+        }
+
+        private void addBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void generatePlan_Click(object sender, EventArgs e)
+        {
+            shuffleStudents();
+        }
+
+        private void importStudents_Click(object sender, EventArgs e)
+        {
+            getStudents();
+        }
+
+        private void addStudent_Click(object sender, EventArgs e)
+        {
+            studentsBox.Items.Add(addBox.Text);
+            addBox.Text = "";
+        }
+
+        private void clearStudents_Click(object sender, EventArgs e)
+        {
+            students.Clear();
+            shuffledBox.Items.Clear();
+            studentsBox.Items.Clear();
+        }
+
+        private void addBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                studentsBox.Items.Add(addBox.Text);
+                addBox.Text = "";
+            }
+        }
+
+        private void studentsBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = studentsBox.IndexFromPoint(e.Location);
+            if (index != ListBox.NoMatches)
+            {
+                studentsBox.Items.RemoveAt(index);
+            }
+        }
+    }
+}
